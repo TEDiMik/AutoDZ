4401a121f904616af58c61a39737bfb7ceaf3a26
Сергей <121krolik11@gmail.com>
2020-02-01 14:39:02 +0500

    just comment

--- authorization.py
+++ authorization.py
@@ -6,7 +6,11 @@ def loginbot(login, password):
 
         s = requests.Session()
 
-        url = 'https://auth.ir-tech.ru/core/login?signin=75281d38fa2a79e7fe5699913bc1c535'
+        '''
+        url надо получать на месте
+        Через сетевой город
+        '''
+        url = 'https://auth.ir-tech.ru/core/connect/authorize?client_id=SGOContext&nonce=825e71f2-4cec-4bfb-885d-d4037a703e8a&response_type=code&scope=openid SGOContext&redirect_uri=http%3a%2f%2fsg.lyceum130.ru%2fasp%2fsso%2firtech%2fcallback%2flogin'
 
 
 
@@ -14,7 +18,7 @@ def loginbot(login, password):
                 'idsrv.xsrf': 'iJCm7F7k6ZEdbQj1IR41l4msq6gLs_c8nLlGAmFOCgMrd1tE50e7nzEO3SaPo7p_zIjixn0khBVtQnNTfrk04_nuFd_SYBvi9QJzS9Tb1r0',
                 'username': login,
                 'password': password,
-                'go': ' '
+                'go': ''
         }
         r = s.post(url,data)
         print(r.text)
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+
+
+#Нужно спарсить сылку с сетвого города 
+#потом с этой ссылки произвести авторизацию
+#и мы попадем в сетевой город
+
 import authorization
 
-authorization.loginbot('TediH1','13245156')
+authorization.loginbot('login','password')
 
